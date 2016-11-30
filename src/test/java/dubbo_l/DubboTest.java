af559928ac0f806935e338ea2269362e34d615f1
yongheng.wang <yongheng@yongheng-PC>
2016-11-30 16:12:45 +0800

    提供方修改

--- src/test/java/dubbo_l/DubboTest.java
+++ src/test/java/dubbo_l/DubboTest.java
@@ -1,24 +1,13 @@
 package dubbo_l;
 
-import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
-import com.dubbo.test.DubboProviderTestService;
 
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration({ "/spring-context.xml" })
 public class DubboTest {
 
-	@Autowired
-	private DubboProviderTestService dubboProviderTestService;
-	
-	@Test
-	public void testProvider(){
-		System.out.println("===测试dubbo==="+dubboProviderTestService.getHelloWord());
-	}
 	
 }
